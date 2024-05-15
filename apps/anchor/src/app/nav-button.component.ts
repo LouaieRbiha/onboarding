b11ae42762b6f7be99f36a23e0e901eeb3d3ef8a
HARMAZ <aymane.harmaz@capgemini.com>
2024-05-15 14:47:30 +0100

    Adding navigation and smooth scrolling

--- apps/anchor/src/app/nav-button.component.ts
+++ apps/anchor/src/app/nav-button.component.ts
@@ -1,10 +1,11 @@
 /* eslint-disable @angular-eslint/component-selector */
 import { Component, Input } from '@angular/core';
+import { RouterLink, RouterLinkWithHref } from '@angular/router';
 @Component({
   selector: 'nav-button',
   standalone: true,
   template: `
-    <a [href]="href">
+    <a [routerLink]="href" [fragment]="section">
       <ng-content></ng-content>
     </a>
   `,
@@ -12,7 +13,9 @@ import { Component, Input } from '@angular/core';
   host: {
     class: 'block w-fit border border-red-500 rounded-md p-4 m-2',
   },
+  imports: [RouterLink, RouterLinkWithHref]
 })
 export class NavButtonComponent {
   @Input() href = '';
+  @Input() section!: string;
 }
