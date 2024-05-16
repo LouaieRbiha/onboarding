e9e1189446f2fafc8ef292d16e4bc313c2773d2d
HARMAZ <aymane.harmaz@capgemini.com>
2024-05-16 16:15:05 +0100

    adding custom directive that mimics the behavior of the ngFor built-in directive

--- apps/directives/structural/src/app/app.component.ts
+++ apps/directives/structural/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { CustomIfDirective } from './directives/CustomIf.directive';
+import { CustomForDirective } from './directives/CustomFor.directive';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, FormsModule, CustomIfDirective],
+  imports: [RouterModule, FormsModule, CustomIfDirective, CustomForDirective],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
@@ -14,4 +15,5 @@ import { CustomIfDirective } from './directives/CustomIf.directive';
 export class AppComponent {
   title = 'structural';
   isClicked = false;
+  items = ["item1", "item2", "item3"];
 }
