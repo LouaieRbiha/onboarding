a959d7300783f6c0b9da05955f5cce69938ab60e
HARMAZ <aymane.harmaz@capgemini.com>
2024-05-17 11:57:10 +0100

    adding custom directive for swaping the display of 2 components every 5 seconds

--- apps/directives/structural/src/app/app.component.ts
+++ apps/directives/structural/src/app/app.component.ts
@@ -3,10 +3,14 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { CustomIfDirective } from './directives/CustomIf.directive';
 import { CustomForDirective } from './directives/CustomFor.directive';
+import { FirstComponent } from './components/first.component';
+import { SecondComponent } from './components/second.component';
+import { CommonModule } from '@angular/common';
+import { SwapDirective } from './directives/Swap.directive';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, FormsModule, CustomIfDirective, CustomForDirective],
+  imports: [RouterModule, FormsModule, CustomIfDirective, CustomForDirective, SwapDirective, FirstComponent, SecondComponent, CommonModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
---
+++ apps/directives/structural/src/app/components/second.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    selector: "second-component",
+    template: `<div>second one</div>`
+})
+export class SecondComponent {
+
+}
